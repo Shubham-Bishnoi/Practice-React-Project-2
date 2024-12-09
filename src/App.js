a7ddb910f50ac79c792d24bf4a9618c769bf9d53
Shubham-Bishnoi <shubhambis9oi@gmail.com>
2024-12-09 15:00:23 +0530

    Inital Commit for React practice project 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //Initial State Variable
+  const [todos, setTodos] = useState([{}]);
+
+  const [camel, setCamel] = useState(0);
+  const [newTodo, setNewTodo] = useState({ title: "", description: "" });
+
+  // 1st component
+  function addTodos() {
+    if (newTodo.title && newTodo.description) {
+      setTodos([
+        ...todos,
+        { title: newTodo.title, description: newTodo.description, done: false },
+      ]);
+      setNewTodo({ tittle: "", description: "" });
+    } else {
+      alert("Please fill in the both fields.");
+    }
+  }
+
+  // Toggle the "done" status of a todo
+  function toggleDone(index) {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(updatedTodos);
+  }
+
+  // Delete a todo
+  function deleteTodo(index) {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {camel}
+      <button onClick={() => handleButton("videoId")}>
+        INcrease value of k by 1
+      </button>
+      {camel === 30 ? (
+        <h1>My todo Application</h1>
+      ) : (
+        <h1 style={{ color: "red" }}>My todo Application</h1>
+      )}
+      video
+      {/* Form to add new todos */}
+      <div className="todo-form">
+        <input
+          type="text"
+          placeholder="Tittle"
+          value={newTodo.title}
+          onChange={(e) => setNewTodo({ ...newTodo, title: e.target.value })}
+        ></input>
+        <input
+          type="text"
+          placeholder="Description"
+          value={newTodo.description}
+          onChange={(e) =>
+            setNewTodo({ ...newTodo, description: e.target.value })
+          }
+        ></input>
+        <button onClick={addTodos}>Add Todo</button>
+      </div>
+      {/* Display the list of todos */}
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.done ? "done" : ""}>
+            <h2>{todo.title}</h2>
+            <p>{todo.description}</p>
+            <div className="actions">
+              <button onClick={() => toggleDone(index)}>
+                {todo.done ? "Undo" : "Done"}
+              </button>
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
